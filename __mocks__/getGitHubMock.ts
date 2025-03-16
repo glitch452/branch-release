9a99a8483fdea1de07aa839003ee359a3d288afc
glitch452 <894128+glitch452@users.noreply.github.com>
2025-03-16 16:12:02 -0700

    chore: Update dependencies

--- __mocks__/getGitHubMock.ts
+++ __mocks__/getGitHubMock.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-type-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
 import { GitHub } from 'src/types/GitHub.js';
